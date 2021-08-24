adf44036768e0aee3cad187aefc7596d39ba545a
Andrew <rolyata47@gmail.com>
2021-08-24 11:58:35 -0500

    Committing Streams.java

---
+++ Streams.java
@@ -0,0 +1,13 @@
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Streams {
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[] {1,2,3,4,5,6,7,8,9};
+        List<Integer> lst = Arrays.asList(arr);  
+        lst.stream().filter(i -> i > 4).forEach(i -> System.out.println(i));
+    }
+    
+}
