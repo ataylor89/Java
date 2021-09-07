a7822255fe4321adc161d018174317e9a917fe42
Andrew <rolyata47@gmail.com>
2021-09-06 21:48:25 -0500

    Added code to Streams.java

--- Streams.java
+++ Streams.java
@@ -8,6 +8,10 @@ public class Streams {
         Integer[] arr = new Integer[] {1,2,3,4,5,6,7,8,9};
         List<Integer> lst = Arrays.asList(arr);  
         lst.stream().filter(i -> i > 4).forEach(i -> System.out.println(i));
+        System.out.println(lst.stream().filter(i -> i > 4).mapToInt(i -> i.intValue()).sum());
+        String ls = "ls";
+        String[] words = ls.split(" ");
+        System.out.println(words[0]);
     }
     
 }
