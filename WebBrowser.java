7b99f207e0dc295b3befa1548334c800dfb6f6d6
Andrew <rolyata47@gmail.com>
2021-09-24 09:00:09 -0500

    "Using JavaFx. Created a TabPane for tabs"

--- WebBrowser.java
+++ WebBrowser.java
@@ -1,54 +1,86 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Scene;
+import javafx.scene.web.WebView;
+import javafx.scene.web.WebEngine;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.Tab;
+import java.util.Optional;
 
-public class WebBrowser extends JFrame implements ActionListener {
+public class WebBrowser extends Application {
 	
-	private JMenuBar menuBar;
-	private JMenu web;
-	private JMenuItem openURL, openHomePage, openSearchEngine;
-	private JPanel panel;	
-	private JEditorPane editorPane;
+	private BorderPane root;
+	private TabPane tabPane;
+	private MenuBar menuBar;
+	private Menu webMenu;
+	private MenuItem openURL, openFile, openHomePage, openSearchPage, exit;
+	private String home = "https://docs.oracle.com/javase/8/docs/api/";
 
 	public WebBrowser() {
-		super("Web Browser");
+	
+	}
+	
+	private void createTab(String title, String url) {
+		WebView browser = new WebView();
+		Tab tab = new Tab(title, browser);
+		tabPane.getTabs().add(tab);
+		browser.getEngine().load(url);	
+		tabPane.getSelectionModel().select(tab);
 	}
 
-	public void createAndShowGui() {
-		setSize(1000, 800);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		menuBar = new JMenuBar();
-		web = new JMenu("Web");
-		openURL = new JMenuItem("URL");
-		openURL.addActionListener(this);
-		openHomePage = new JMenuItem("Home");
-		openHomePage.addActionListener(this);
-		openSearchEngine = new JMenuItem("Search");
-		openSearchEngine.addActionListener(this);
-		web.add(openURL);
-		web.add(openHomePage);
-		web.add(openSearchEngine);
-		menuBar.add(web);
-		setJMenuBar(menuBar);
-		panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		try {
-			editorPane = new JEditorPane("https://docs.oracle.com/javase/8/docs/api/");
-			panel.add(editorPane);
-		} catch (IOException e) {
-			System.err.println(e);
-		}
-		add(panel);
-		setVisible(true);
+	private void createTab(String url) {
+		createTab("Tab", url);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-
+	@Override
+	public void start(Stage stage) {
+		root = new BorderPane();
+		menuBar = new MenuBar();
+		webMenu = new Menu("Web");
+		openURL = new MenuItem("URL");
+		openURL.setOnAction(e -> {
+			TextInputDialog dialog = new TextInputDialog();
+			dialog.setTitle("URL");
+			dialog.setHeaderText("URL:");
+			dialog.getDialogPane().setMinWidth(500);
+			Optional<String> url = dialog.showAndWait();	
+			if (url.isPresent()) 
+				createTab(url.get());
+		});
+		openHomePage = new MenuItem("Home");
+		openHomePage.setOnAction(e -> {
+			createTab("Home Page", home);
+		});
+		openSearchPage = new MenuItem("Search");
+		exit = new MenuItem("Exit");
+		exit.setOnAction(e -> stage.close());
+		webMenu.getItems().addAll(openURL, openHomePage, openSearchPage, exit);
+		menuBar.getMenus().add(webMenu);
+		root.setTop(menuBar);
+		tabPane = new TabPane();
+		createTab("Home Page", home);
+		root.setCenter(tabPane);
+		Scene scene = new Scene(root, 1200, 1200);
+		stage.setScene(scene);
+		stage.setOnCloseRequest(e -> {
+			Platform.exit();
+			System.exit(0);	
+		});
+		stage.sizeToScene();
+		stage.show();
 	}
+
 
 	public static void main(String[] args) {
-		WebBrowser webBrowser = new WebBrowser();
-		webBrowser.createAndShowGui();
+		launch(args);
 	}
 }
