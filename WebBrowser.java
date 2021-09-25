e4d2cca86a979c0ecc62df005eef468bbb0ecb44
Andrew <rolyata47@gmail.com>
2021-09-24 19:54:31 -0500

    "Added features for opening HTML, text, and image files"

--- WebBrowser.java
+++ WebBrowser.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.stage.FileChooser;
 import javafx.scene.Scene;
 import javafx.scene.web.WebView;
 import javafx.scene.web.WebEngine;
@@ -14,9 +15,17 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class WebBrowser extends Application {
 	
@@ -24,12 +33,12 @@ public class WebBrowser extends Application {
 	private TabPane tabPane;
 	private MenuBar menuBar;
 	private Menu webMenu;
-	private MenuItem openURL, openFile, openHomePage, openSearchPage, exit;
+	private MenuItem openURL, openHTMLFile, openTextFile, openImage, openHomePage, openSearchPage, exit;
 	private String home = "https://docs.oracle.com/javase/8/docs/api/";
 
 	public WebBrowser() {}
 	
-	private void createTab(String url) {
+	private void openURL(String url) {
 		WebView browser = new WebView();
 		browser.getEngine().load(url);	
 		String title = "Tab";
@@ -43,12 +52,44 @@ public class WebBrowser extends Application {
 		tabPane.getSelectionModel().select(tab);
 	}
 
+	private void openHTMLFile(File file) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String html = reader.lines().collect(Collectors.joining("\n"));
+			WebView browser = new WebView();
+			browser.getEngine().loadContent(html);
+			Tab tab = new Tab(file.getName(), browser);
+			tabPane.getTabs().add(tab);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	private void openTextFile(File file) {
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			String text = reader.lines().collect(Collectors.joining("\n"));
+			TextArea textArea = new TextArea(text);
+			Tab tab = new Tab(file.getName(), textArea);
+			tabPane.getTabs().add(tab);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+
+	private void openImage(File file) {
+		Image image = new Image(file.toURI().toString());
+		ImageView imageView = new ImageView(image);
+		imageView.setFitWidth(1150);
+		imageView.setPreserveRatio(true);
+		Tab tab = new Tab(file.getName(), imageView);
+		tabPane.getTabs().add(tab);
+	}
+
 	@Override
 	public void start(Stage stage) {
 		root = new BorderPane();
 		menuBar = new MenuBar();
 		webMenu = new Menu("Web");
-		openURL = new MenuItem("URL");
+		openURL = new MenuItem("Open URL");
 		openURL.setOnAction(e -> {
 			TextInputDialog dialog = new TextInputDialog();
 			dialog.setTitle("URL");
@@ -56,20 +97,44 @@ public class WebBrowser extends Application {
 			dialog.getDialogPane().setMinWidth(500);
 			Optional<String> url = dialog.showAndWait();	
 			if (url.isPresent()) 
-				createTab(url.get());
+				openURL(url.get());
 		});
-		openHomePage = new MenuItem("Home");
+		openHTMLFile = new MenuItem("Open HTML file");
+		openHTMLFile.setOnAction(e -> {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Open file");
+			File file = fileChooser.showOpenDialog(stage);
+			if (file != null)
+				openHTMLFile(file);
+		});
+		openTextFile = new MenuItem("Open text file");
+		openTextFile.setOnAction(e -> {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Open file");
+			File file = fileChooser.showOpenDialog(stage);
+			if (file != null)
+				openTextFile(file);
+		});
+		openImage = new MenuItem("Open image");
+		openImage.setOnAction(e -> {
+			FileChooser fileChooser = new FileChooser();
+			fileChooser.setTitle("Open image");
+			File file = fileChooser.showOpenDialog(stage);
+			if (file != null)
+				openImage(file);
+		});
+		openHomePage = new MenuItem("Open home page");
 		openHomePage.setOnAction(e -> {
-			createTab(home);
+			openURL(home);
 		});
-		openSearchPage = new MenuItem("Search");
+		openSearchPage = new MenuItem("Open search page");
 		exit = new MenuItem("Exit");
 		exit.setOnAction(e -> stage.close());
-		webMenu.getItems().addAll(openURL, openHomePage, openSearchPage, exit);
+		webMenu.getItems().addAll(openURL, openHTMLFile, openTextFile, openImage, openHomePage, openSearchPage, exit);
 		menuBar.getMenus().add(webMenu);
 		root.setTop(menuBar);
 		tabPane = new TabPane();
-		createTab(home);
+		openURL(home);
 		root.setCenter(tabPane);
 		Scene scene = new Scene(root, 1200, 1200);
 		stage.setScene(scene);
